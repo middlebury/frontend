2035228a4c8de008cb8a10a47941ece32faf670f
Zeb Pykosz <zeb.apy@gmail.com>
2017-03-17 08:42:09 -0400

    create mission selector js for replacing select input with button
    this is needed because you cannot prevent default event on a select
    input to stop the options from showing up on click

---
+++ src/js/mission-selector.js
@@ -0,0 +1,34 @@
+import Modal from './modal';
+
+function createButton(select) {
+  const button = document.createElement('button');
+
+  button.classList = select.classList;
+
+  const options = select.querySelectorAll('option');
+
+  if (options && options.length) {
+    button.appendChild(document.createTextNode(options[0].innerText));
+  }
+
+  return button;
+}
+
+function main(select) {
+  select.style.display = 'none';
+
+  const button = createButton(select);
+
+  select.parentElement.appendChild(button);
+
+  const modal = new Modal('#mission-modal');
+
+  button.addEventListener('click', e => {
+    e.preventDefault();
+    modal.open();
+  });
+}
+
+const select = document.querySelector('.js-mission-selector');
+
+main(select);
