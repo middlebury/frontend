2574d006ef51ecfad0bd7ed3fc25c01bfb5cc065
Zeb Pykosz <zeb.apy@gmail.com>
2017-03-27 12:44:54 -0400

    finish carousel

--- src/js/carousel.js
+++ src/js/carousel.js
@@ -1,4 +1,6 @@
+import enquire from 'enquire.js';
 import forEach from './utils/forEach';
+import optimizedResize from './utils/optimizedResize';
 
 const selector = {
   CAROUSEL: '[data-carousel]',
@@ -8,14 +10,14 @@ const selector = {
   ITEM: '[data-carousel-item]'
 };
 
+const defaultConfig = {
+  show: 1,
+  loop: false,
+  minWidth: 0
+};
+
 class Carousel {
-  constructor(
-    elem,
-    config = {
-      show: 1,
-      loop: false
-    }
-  ) {
+  constructor(elem, config = defaultConfig) {
     this.elem = elem;
 
     this.config = this.getConfig(config);
@@ -26,23 +28,86 @@ class Carousel {
     this.nextBtn = this.elem.querySelector(selector.NEXT_BUTTON);
     this.prevBtn = this.elem.querySelector(selector.PREV_BUTTON);
 
-    this.show = this.getShow();
-
+    this.show = 1;
     this.index = 1;
     this.itemCount = 0;
     this.itemWidth = 0;
-    this.offset = 0;
 
     this.handlePrevClick = this.handlePrevClick.bind(this);
     this.handleNextClick = this.handleNextClick.bind(this);
+    this.handleWindowResize = this.handleWindowResize.bind(this);
 
-    this.init();
+    this.enabled = false;
+
+    if (this.config.minWidth && this.config.minWidth > 0) {
+      enquire.register(`screen and (min-width: ${this.config.minWidth}px)`, {
+        match: () => {
+          this.enable();
+        },
+        unmatch: () => {
+          this.disable();
+        }
+      });
+    }
+  }
+
+  enable() {
+    if (!this.items || this.enabled) {
+      return;
+    }
+
+    this.enabled = true;
+
+    this.addListeners();
+
+    this.reset();
+  }
+
+  disable() {
+    if (!this.enabled) {
+      return;
+    }
+
+    // remove event handlers from next and previous controls
+    if (this.nextBtn && this.prevBtn) {
+      this.nextBtn.removeEventListener('click', this.handleNextClick);
+      this.prevBtn.removeEventListener('click', this.handlePrevClick);
+    }
+
+    // remove window resize checker
+    window.removeEventListener('optimizedResize', this.handleWindowResize);
+
+    // remove minWidth inline style from carousel items
+    forEach(this.items, item => {
+      item.style.minWidth = '';
+    });
+
+    // set the widget to disabled
+    this.enabled = false;
+  }
+
+  reset() {
+    this.index = 1;
+    this.show = this.getShow();
+    this.itemCount = this.items.length;
+    this.itemWidth = this.getItemWidth();
+    this.positionContainer(this.index);
+    this.updateControls();
+  }
+
+  addListeners() {
+    if (this.nextBtn && this.prevBtn) {
+      this.nextBtn.addEventListener('click', this.handleNextClick);
+      this.prevBtn.addEventListener('click', this.handlePrevClick);
+    }
+
+    window.addEventListener('optimizedResize', this.handleWindowResize);
   }
 
   getConfig(defaultConfig) {
-    const configAttr = this.elem.getAttribute('data-carousel');
-
     let config = {};
+
+    const configAttr = this.elem.getAttribute('data-carousel');
 
     if (configAttr) {
       try {
@@ -56,48 +121,25 @@ class Carousel {
     return Object.assign({}, defaultConfig, config);
   }
 
+  isLastItemVisible() {
+    return this.index + (this.show - 1) >= this.itemCount;
+  }
+
   getShow() {
     let show = 1;
 
-    if (typeof this.config.show == 'object') {
-      Object.keys(this.config.show, key => {
-        if (window.matchMedia(`(min-width: ${key})`).matches) {
+    if (typeof this.config.show === 'object') {
+      for (let key in this.config.show) {
+        if (window.matchMedia(`(min-width: ${key}px)`).matches) {
           show = this.config.show[key];
         }
-      });
+      }
     }
     else {
       show = this.config.show;
     }
 
-    return this.totalItems > show ? this.totalItems : show;
-  }
-
-  isLastItemVisible() {
-    let x = this.index + (this.show - 1) >= this.itemCount;
-    console.log(x);
-    return x;
-  }
-
-  init() {
-    if (!this.items) {
-      return;
-    }
-
-    this.addListeners();
-
-    this.itemCount = this.items.length;
-
-    this.setItemWidth();
-
-    this.updateControls();
-  }
-
-  addListeners() {
-    if (this.nextBtn && this.prevBtn) {
-      this.nextBtn.addEventListener('click', this.handleNextClick);
-      this.prevBtn.addEventListener('click', this.handlePrevClick);
-    }
+    return this.itemCount > this.show ? this.itemCount : show;
   }
 
   updateControls() {
@@ -112,11 +154,14 @@ class Carousel {
     }
   }
 
-  setItemWidth() {
-    this.itemWidth = this.items[0].offsetWidth;
+  getItemWidth() {
+    const item = this.items[0];
+    return item.getBoundingClientRect().width || item.offsetWidth;
   }
 
-  watchResize() {}
+  handleWindowResize() {
+    this.reset();
+  }
 
   next() {
     this.positionContainer(this.index + 1);
@@ -127,7 +172,6 @@ class Carousel {
   }
 
   positionContainer(index) {
-    console.log(index);
     const width = (index - 1) * this.itemWidth;
     this.index = index;
     this.container.style.transform = `translateX(-${width}px)`;
