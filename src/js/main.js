b5c77e918d255e2c9aab51f288fddc46dcff7517
Zeb Pykosz <zeb.apy@gmail.com>
2017-05-12 14:44:06 -0400

    set up publications swiper

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,27 @@
 import './toggler';
 import './nav';
 import './mission-selector';
-import './carousel';
+// import './carousel';
+
+import Swiper from 'swiper';
+
+const pubSwiperElem = document.querySelector('.js-publications-swiper');
+
+if (pubSwiperElem) {
+  const publicationsSwiper = new Swiper(pubSwiperElem, {
+    a11y: true,
+    buttonDisabledClass: 'publications__button--disabled',
+    slidesPerView: 4,
+    nextButton: '.js-publications-next-button',
+    prevButton: '.js-publications-prev-button',
+    breakpoints: {
+      580: {
+        slidesPerView: 1
+      },
+      960: {
+        slidesPerView: 2
+      }
+    }
+  });
+}
+
