54d4bc2d595baa7605951dc54a757e93e323a2f7
Zeb Pykosz <zeb.apy@gmail.com>
2017-05-25 10:16:03 -0400

    set up swiper for home page

---
+++ src/js/config.js
@@ -0,0 +1,11 @@
+const config = {
+  breakpoints: {
+    xs: 0,
+    sm: 580,
+    md: 960,
+    lg: 1200,
+    xl: 1440
+  }
+};
+
+export default config;
---
+++ src/js/program-spotlight.js
@@ -0,0 +1,40 @@
+import enquire from 'enquire.js';
+import Swiper from 'swiper';
+
+import config from './config';
+
+const mq = `screen and (min-width: ${config.breakpoints.sm}px)`;
+
+let swiperInstance;
+
+const swiperConfig = {
+  buttonDisabledClass: 'button--disabled',
+  slidesPerView: 3,
+  nextButton: '.js-program-spotlight-next-button',
+  prevButton: '.js-program-spotlight-prev-button',
+  breakpoints: {
+    [config.breakpoints.sm]: {
+      slidesPerView: 1
+    },
+    [config.breakpoints.md]: {
+      slidesPerView: 2
+    }
+  }
+};
+
+function match() {
+  const programSwiperElem = document.querySelector('.js-program-spotlight');
+  swiperInstance = new Swiper(programSwiperElem, swiperConfig);
+}
+
+function unmatch() {
+  swiperInstance.destroy(
+    true, // deleteInstancee
+    true // cleanupStyles
+  );
+}
+
+enquire.register(mq, {
+  match,
+  unmatch
+});
