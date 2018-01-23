08705e7c0ea39a379c6d16f666c0cbd88b73ed1c
Zeb Pykosz <zeb.apy@gmail.com>
2018-01-23 16:22:26 -0500

    rework slideshow for new design

--- src/js/slideshow.js
+++ src/js/slideshow.js
@@ -4,6 +4,8 @@ const elems = document.querySelectorAll('.js-slideshow');
 
 if (elems) {
   [].forEach.call(elems, elem => {
+    const slideshowCaption = elem.querySelector('.js-slideshow-caption');
+
     const slideshow = new Swiper(elem, {
       pagination: {
         el: elem.querySelector('.js-slideshow-pagination'),
@@ -20,6 +22,25 @@ if (elems) {
       lazy: {
         loadPrevNext: true
       },
+      init: false
     });
+
+    var captions = elem.querySelectorAll('figcaption');
+
+    [].forEach.call(captions, c => (c.style.display = 'none'));
+
+    slideshowCaption.innerText = captions[0].innerText;
+
+    slideshow.on('slideChangeTransitionEnd', function () {
+      var activeSlide = elem.querySelector('.swiper-slide-active');
+      var caption = activeSlide.querySelector('figcaption');
+      slideshowCaption.innerText = caption.innerText;
+    });
+
+    slideshow.on('slideChangeTransitionStart', function () {
+      slideshowCaption.innerText = '';
+    });
+
+    slideshow.init();
   });
 }
