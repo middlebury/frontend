2cfbdabb336937d0323ea52d116ad8c22d7f72fa
Zeb Pykosz <zeb.apy@gmail.com>
2018-01-23 15:18:56 -0500

    upgrade swiper to v4

--- src/js/slideshow.js
+++ src/js/slideshow.js
@@ -5,15 +5,21 @@ const elems = document.querySelectorAll('.js-slideshow');
 if (elems) {
   [].forEach.call(elems, elem => {
     const slideshow = new Swiper(elem, {
-      pagination: elem.querySelector('.js-slideshow-pagination'),
-      nextButton: elem.querySelector('.js-slideshow-next'),
-      prevButton: elem.querySelector('.js-slideshow-prev'),
-      buttonDisabledClass: 'is-disabled',
-      paginationType: 'fraction',
+      pagination: {
+        el: elem.querySelector('.js-slideshow-pagination'),
+        type: 'fraction'
+      },
+      navigation: {
+        nextEl: elem.querySelector('.js-slideshow-next'),
+        prevEl: elem.querySelector('.js-slideshow-prev'),
+        disabledClass: 'is-disabled'
+      },
       a11y: true,
-      lazyLoading: true,
+      autoHeight: true,
       preloadImages: false,
-      lazyLoadingInPrevNext: true
+      lazy: {
+        loadPrevNext: true
+      },
     });
   });
 }
