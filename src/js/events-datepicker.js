eac4f8156a912f7e859d0811ab5a6235357693e3
Ian McBride <imcbride@middlebury.edu>
2018-02-23 13:20:03 -0500

    Check that the flatpickr function exists. For #133.

--- src/js/events-datepicker.js
+++ src/js/events-datepicker.js
@@ -1,10 +1,12 @@
-var urlParts = drupalSettings.path.currentPath.split('/');
+if (typeof flatpickr !== 'undefined') {
+  var urlParts = drupalSettings.path.currentPath.split('/');
 
-flatpickr('#midd-event-date-widget', {
-  inline: true,
-  defaultDate: urlParts[2] ? urlParts[2] : 'today',
-  minDate: 'today',
-  onChange: function (selectedDates, dateStr, instance) {
-    location.href = drupalSettings.path.baseUrl + 'events/all/' + dateStr;
-  }
-});
+  flatpickr('#midd-event-date-widget', {
+    inline: true,
+    defaultDate: urlParts[2] ? urlParts[2] : 'today',
+    minDate: 'today',
+    onChange: function (selectedDates, dateStr, instance) {
+      location.href = drupalSettings.path.baseUrl + 'events/all/' + dateStr;
+    }
+  });
+}
