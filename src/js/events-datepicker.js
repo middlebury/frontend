53c4dc8feafe97b9319e9362473589a17b531770
Ian McBride <imcbride@middlebury.edu>
2018-02-23 13:32:23 -0500

    Handle the case where drupalSettings is undefined. For #133.

--- src/js/events-datepicker.js
+++ src/js/events-datepicker.js
@@ -1,14 +1,21 @@
 import flatpickr from 'flatpickr';
 
 if (typeof flatpickr !== 'undefined') {
-  var urlParts = drupalSettings.path.currentPath.split('/');
+  var eventsBase = '/';
+  var eventsPath = '/';
+
+  if (typeof drupalSettings !== 'undefined') {
+    eventsBase = drupalSettings.path.baseUrl;
+    eventsPath = drupalSettings.path.currentPath;
+  }
+  var urlParts = eventsPath.split('/');
 
   flatpickr('#midd-event-date-widget', {
     inline: true,
     defaultDate: urlParts[2] ? urlParts[2] : 'today',
     minDate: 'today',
     onChange: function (selectedDates, dateStr, instance) {
-      location.href = drupalSettings.path.baseUrl + 'events/all/' + dateStr;
+      location.href = eventsBase + 'events/all/' + dateStr;
     }
   });
 }
