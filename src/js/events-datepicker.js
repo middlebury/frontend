f310f7b4ba90d90f3ad1341c51d3df8c110d837b
Ian McBride <imcbride@middlebury.edu>
2018-02-27 10:14:02 -0500

    Fix issues identified by @zebapy for review. For #133.

--- src/js/events-datepicker.js
+++ src/js/events-datepicker.js
@@ -1,21 +1,19 @@
 import flatpickr from 'flatpickr';
 
-if (typeof flatpickr !== 'undefined') {
-  var eventsBase = '/';
-  var eventsPath = '/';
+let eventsBase = '/';
+let eventsPath = '/';
 
-  if (typeof drupalSettings !== 'undefined') {
-    eventsBase = drupalSettings.path.baseUrl;
-    eventsPath = drupalSettings.path.currentPath;
-  }
-  var urlParts = eventsPath.split('/');
-
-  flatpickr('#midd-event-date-widget', {
-    inline: true,
-    defaultDate: urlParts[2] ? urlParts[2] : 'today',
-    minDate: 'today',
-    onChange: function (selectedDates, dateStr, instance) {
-      location.href = eventsBase + 'events/all/' + dateStr;
-    }
-  });
+if (typeof drupalSettings !== 'undefined') {
+  eventsBase = drupalSettings.path.baseUrl;
+  eventsPath = drupalSettings.path.currentPath;
 }
+const urlParts = eventsPath.split('/');
+
+flatpickr('#midd-event-date-widget', {
+  inline: true,
+  defaultDate: urlParts[2] ? urlParts[2] : 'today',
+  minDate: 'today',
+  onChange: function (selectedDates, dateStr, instance) {
+    location.href = eventsBase + 'events/all/' + dateStr;
+  }
+});
