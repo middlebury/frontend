ddb78d0093edcccfebf05ec461500a5021786d46
Zeb Pykosz <zeb.apy@gmail.com>
2018-02-27 16:11:31 -0500

    switch flatpickr to pikaday

--- src/js/events-datepicker.js
+++ src/js/events-datepicker.js
@@ -1,21 +1,38 @@
-import flatpickr from 'flatpickr';
+import Pikaday from 'Pikaday';
 
-if (typeof flatpickr !== 'undefined') {
-  var eventsBase = '/';
-  var eventsPath = '/';
+let eventsBase = '/';
+let eventsPath = '/';
 
-  if (typeof drupalSettings !== 'undefined') {
-    eventsBase = drupalSettings.path.baseUrl;
-    eventsPath = drupalSettings.path.currentPath;
-  }
-  var urlParts = eventsPath.split('/');
-
-  flatpickr('#midd-event-date-widget', {
-    inline: true,
-    defaultDate: urlParts[2] ? urlParts[2] : 'today',
-    minDate: 'today',
-    onChange: function (selectedDates, dateStr, instance) {
-      location.href = eventsBase + 'events/all/' + dateStr;
-    }
-  });
+if (typeof drupalSettings !== 'undefined') {
+  eventsBase = drupalSettings.path.baseUrl;
+  eventsPath = drupalSettings.path.currentPath;
 }
+
+const urlParts = eventsPath.split('/');
+
+const defaultDate = urlParts[2] ? urlParts[2] : new Date();
+
+const pad = n => (n >= 10 ? n : '0' + n);
+
+const picker = new Pikaday({
+  field: document.querySelector('.js-events-datepicker'),
+  bound: false,
+  minDate: new Date(),
+  showDaysInNextAndPreviousMonths: true,
+  defaultDate,
+  setDefaultDate: !!defaultDate,
+  // format: 'YYYY/MM/DD',
+  onSelect(date) {
+    let day = date.getDate();
+    let month = date.getMonth() + 1;
+
+    day = pad(day);
+    month = pad(month);
+
+    const year = date.getFullYear();
+
+    const dateStr = `${year}-${month}-${day}`;
+
+    location.href = eventsBase + 'events/all/' + dateStr;
+  }
+});
