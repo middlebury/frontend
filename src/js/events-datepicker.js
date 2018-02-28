b5615350ffdff224c24604a048abfff17a5808fb
Zeb Pykosz <zeb.apy@gmail.com>
2018-02-28 12:37:56 -0500

    get date setting from url to work for pikaday

--- src/js/events-datepicker.js
+++ src/js/events-datepicker.js
@@ -1,5 +1,32 @@
 import Pikaday from 'pikaday';
 
+const pad = n => (n >= 10 ? n : '0' + n);
+
+const getDateObj = dateStr => {
+  const dateParts = dateStr.split('-');
+  let [year, month, day] = dateParts;
+
+  year = parseInt(year, 10);
+  month = parseInt(month, 10) - 1;
+  day = parseInt(day, 10);
+
+  return {day, month, year};
+};
+
+const dateToStr = date => {
+  let day = date.getDate();
+  let month = date.getMonth() + 1;
+
+  day = pad(day);
+  month = pad(month);
+
+  const year = date.getFullYear();
+
+  const dateStr = `${year}-${month}-${day}`;
+
+  return dateStr;
+};
+
 let eventsBase = '/';
 let eventsPath = '/';
 
@@ -9,10 +36,17 @@ if (typeof drupalSettings !== 'undefined') {
 }
 
 const urlParts = eventsPath.split('/');
+const dateStr = urlParts[2];
 
-const defaultDate = urlParts[2] ? urlParts[2] : new Date();
+let defaultDate = null;
 
-const pad = n => (n >= 10 ? n : '0' + n);
+if (dateStr) {
+  const {year, month, day} = getDateObj(dateStr);
+
+  // have to pass date object instead of string
+  // https://github.com/dbushell/Pikaday/issues/764#issuecomment-360286792
+  defaultDate = new Date(year, month, day);
+}
 
 const picker = new Pikaday({
   field: document.querySelector('.js-events-datepicker'),
@@ -20,17 +54,8 @@ const picker = new Pikaday({
   minDate: new Date(),
   defaultDate,
   setDefaultDate: !!defaultDate,
-  // format: 'YYYY/MM/DD',
   onSelect(date) {
-    let day = date.getDate();
-    let month = date.getMonth() + 1;
-
-    day = pad(day);
-    month = pad(month);
-
-    const year = date.getFullYear();
-
-    const dateStr = `${year}-${month}-${day}`;
+    const dateStr = dateToStr(date);
 
     location.href = eventsBase + 'events/all/' + dateStr;
   }
